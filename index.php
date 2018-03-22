44ae31d526ef624e67239e27c9d2cef19e019de4
foresitegroup <mark@foresitegrp.com>
2018-03-22 17:02:33 -0500

    2018-03-22

--- index.php
+++ index.php
@@ -4,18 +4,18 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width">
-    
+
     <title>Wisconsin Policy Forum<?php if (isset($PageTitle)) echo " | " . $PageTitle; ?></title>
     <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
     <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
-    
+
     <meta name="description" content="">
     <meta name="keywords" content="">
-    
+
     <link href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800" rel="stylesheet">
     <link href="inc/main.css?<?php echo filemtime('inc/main.css'); ?>" rel="stylesheet">
-    
+
     <script type="text/javascript" src="inc/jquery-1.12.4.min.js"></script>
     <script type="text/javascript">
       $(document).ready(function() {
@@ -42,6 +42,8 @@
       </div>
     </header>
 
+    <input type="checkbox" id="toggle-menu" role="button">
+    <label for="toggle-menu"></label>
     <nav id="headnav">
       <ul class="site-width">
         <li>
@@ -76,17 +78,96 @@
         <li><a href="#">News/Blog/Media</a></li>
       </ul>
     </nav>
-    
-    <div class="site-width">
-      <h1>Content (H1)</h1>
-      
-      Bacon ipsum dolor sit amet sausage bacon biltong, salami drumstick hamburger ham hock. Filet mignon ribeye meatball flank tri-tip tongue boudin, doner pig tenderloin. Beef cow turducken pork belly. Corned beef <a href="#">andouille</a> short loin spare ribs. Short ribs frankfurter pig beef ribs. Sausage salami kielbasa cow jowl. Pork ribeye sirloin sausage bacon ham swine turkey biltong tenderloin boudin beef ribs pig hamburger.<br>
-      <br>
-      
-      Pig shankle andouille venison ham frankfurter strip steak ham hock, swine jerky ball tip flank hamburger. Leberkas cow short loin capicola ham hock bresaola. Pig beef ribs salami shankle, ham hock shank flank <a href="fake.pdf">kielbasa</a> sausage hamburger tenderloin. Salami shankle prosciutto sausage pork chop tri-tip. Short loin shankle tail capicola bresaola chuck drumstick pork belly t-bone shoulder hamburger salami corned beef leberkas meatloaf. Corned beef t-bone drumstick jowl shoulder brisket sirloin meatball turkey.<br>
-      <br>
-      
-      Bacon sirloin jowl tail pork loin corned beef sausage ribeye rump. Pork chop spare ribs turkey andouille strip steak. <a href="http://foresitegrp.com">Venison</a> pig bresaola ground round. Leberkas frankfurter pastrami prosciutto bresaola jowl.
+
+    <div id="hero">
+      <div class="box">
+        <div class="site-width">
+          <div class="box-text">
+            <h1><span>Impartial Research</span> for Informed Debate</h1>
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus pulvinar posuere. Aliquam tincidunt mattis diam volutpat tempor. Nulla interdum libero risus, eget hendrerit purus lobortis suscipit. Donec eget libero in eros viverra iaculis. Mauris sed tincidunt felis. Fusce cursus, nibh eu venenatis venenatis, nibh mi vehicula ex, et gravida enim metus id lectus. Mauris quis magna semper, ultrices leo sed, semper tellus. Phasellus rutrum.<br>
+
+            <a href="#" class="button">Request a Presentation</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="ep-header">
+      <div class="site-width">
+        <h1 class="left"><span>Upcoming</span> Events</h1>
+        <h1 class="right"><span>Featured</span> Publication</h1>
+      </div>
+    </div>
+
+    <div id="ep" class="site-width">
+      <div id="events">
+        <div id="pinned">
+          <div class="event">
+            <h2><div>May<div>30</div></div></h2>
+            <h3>The Big Event: A Top-Pinned Event To Generate Buzz</h3>
+            <h4>6:00 - 9:00 PM</h4>
+            The Big Event Location<br>
+            123 Main St., Madison, WI
+          </div>
+        </div>
+
+        <div class="event">
+          <h2><div>Mar<div>06</div></div></h2>
+          <h3>2018 Wisconsin Policy Forum Annual Meeting</h3>
+          <h4>5:00 - 7:00 PM</h4>
+          The Wisconsin Club<br>
+          900 W Wisconsin Ave., Milwaukee, WI
+        </div>
+
+        <div class="event">
+          <h2><div>Mar<div>13</div></div></h2>
+          <h3>Wisconsin Policy Forum Madison Kick-Off Event</h3>
+          <h4>5:00 - 7:00 PM</h4>
+          Brocach Irish Pub & Whiskey Den<br>
+          7 W Main St., Madison, WI
+        </div>
+
+        <div class="event">
+          <h2><div>Apr<div>30</div></div></h2>
+          <h3>2018 Budget Meeting</h3>
+          <h4>5:00 - 7:00 PM</h4>
+          Brocach Irish Pub & Whiskey Den<br>
+          7 W Main St., Madison, WI
+        </div>
+
+        <a href="#" class="button">View Event Calendar</a>
+      </div>
+
+      <div id="publication">
+        <div id="publication-info">
+          <img src="images/2017-03.jpg" alt="">
+        
+          <div>
+            <h2>The Last Mile:</h2>
+            <h3>Connecting Workers to Places of Employment</h3>
+            <h4>March 2017</h4>
+            <h5>Tags:</h5>
+
+            <a href="#" class="tag">Sample Tag</a>
+            <a href="#" class="tag">Sample Tag</a>
+            <a href="#" class="tag">Sample Tag</a>
+            <a href="#" class="tag">Longer Sample Tag</a>
+          </div>
+        </div>
+
+        Our latest research suggests that flexible forms of transit service &mdash; and perhaps new strategies linked to partnerships with ride-hailing companies like Lyft and Uber &mdash; could help address the region's elusive "last mile" problem.<br>
+        <br>
+
+        <h2>Policy Recommendations</h2>
+        <ul>
+          <li>Build on recent efforts to improve transportation connections in the Milwaukee area through shared-ride taxi services.</li>
+          <li>Build on recent efforts to improve transportation connections in the Milwaukee area through shared-ride taxi services.</li>
+          <li>Build on recent efforts to improve transportation connections in the Milwaukee area through shared-ride taxi services.</li>
+        </ul>
+
+        <a href="#" class="button">View Publication</a>
+      </div>
     </div>
 
     <footer>
@@ -96,7 +177,7 @@
 
           <a href="#" class="contact-button">Contact Us</a>
           <a href="#" class="join-button">Join WPF</a>
-          
+
           <div class="social">
             <a href="#"><i class="fab fa-facebook-f"></i></a>
             <a href="#"><i class="fab fa-instagram"></i></a>
@@ -126,6 +207,6 @@
     </footer>
 
     <div id="copyright">&copy; <?php echo date("Y"); ?> Wisconsin Policy Forum</div>
-    
+
   </body>
 </html>
